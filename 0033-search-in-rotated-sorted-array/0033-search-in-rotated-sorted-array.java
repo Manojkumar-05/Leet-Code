40d5fc642408923333d282a406e0275063b7d562
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2023-10-15 08:59:53 +0530

    Time: 0 ms (100%), Space: 40.7 MB (74.33%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,28 @@
 class Solution {
     public int search(int[] arr, int target) {
-        int pivot = findPiv(arr);
-        int ans = BinarySearch(arr, target, 0, pivot);
-        if(ans == -1){
-            return BinarySearch(arr, target, pivot+1, arr.length - 1);
-        }
+        int peak = findPeak(arr);
+        int ans = binarySearch(arr, target, 0, peak);
+        if(ans == -1) return binarySearch(arr, target, peak + 1, arr.length - 1);
         return ans;
     }
-    int findPiv(int[] arr){
-        int start = 0;
-        int end = arr.length - 1;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(mid < end && arr[mid] > arr[mid + 1]) return mid ;
-            if(start < mid && arr[mid] < arr[mid - 1]) return mid-1;
-            if(arr[mid] <= arr[start]) end = mid - 1;
-            else start = mid + 1 ;
-        }
-        return -1;
-
+    public int findPeak(int[] arr){
+    int start = 0, end = arr.length - 1;
+    while(start <= end){
+        int mid = start + (end - start) / 2;
+        if( mid < end && arr[mid] > arr[mid + 1]) return mid;
+        else if ( mid > start && arr[mid] < arr[mid - 1]) return mid - 1;
+        else if (arr[start] >= arr[mid]) end = mid - 1;
+        else if (arr[start] <= arr[mid]) start = mid + 1;
     }
-    int BinarySearch(int[] arr, int target, int start, int end){
-            while (end >= start){
-                int mid = start + (end - start)/2;
-
-                if(target < arr[mid]) end = mid - 1;
-
-                else if(target > arr[mid]) start = mid + 1;
-
-                else return mid;
-
-            }
-            return -1;
-        }
-}   
+    return -1;
+}
+    public int binarySearch(int[] arr, int target, int start, int end){
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] > target) end = mid - 1;
+            else if(arr[mid] < target) start = mid + 1;
+            else return mid;
+         }
+         return -1;
+    }
+}
