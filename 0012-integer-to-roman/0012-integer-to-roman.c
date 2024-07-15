cb3b49041e5783c5e5dac8297bc73b18e6ed4691
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-15 15:07:53 +0530

    Time: 7 ms (29%), Space: 9.2 MB (5.11%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -0,0 +1,18 @@
+char* intToRoman(int num) {
+    int val[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+    const char* sym[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+    int i = 12;
+    char *ans = (char*)malloc(30 * sizeof(char));
+    int idx = 0;
+    while(num){
+        if(num >= val[i]){
+            num -= val[i];
+            ans[idx++] = sym[i][0]; 
+            if(strlen(sym[i]) == 2) ans[idx++] = sym[i][1];
+            printf("%s ", ans);
+        }
+        else i--;
+    }
+    ans[idx] = '\0';
+    return ans;
+}
