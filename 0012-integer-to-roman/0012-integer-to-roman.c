d2ee0fe7cc58dff9c2e4e25a0bf0ca62df33f934
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-15 15:09:21 +0530

    Time: 10 ms (9.89%), Space: 9.4 MB (5.11%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.c
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -2,7 +2,7 @@ char* intToRoman(int num) {
     int val[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
     const char* sym[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
     int i = 12;
-    char *ans = (char*)malloc(30 * sizeof(char));
+    char *ans = (char*)malloc(20 * sizeof(char));
     int idx = 0;
     while(num){
         if(num >= val[i]){
