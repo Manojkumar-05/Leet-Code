962325d279304d501362d9f2fef6959cf3c8e603
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-16 11:51:37 +0530

    Time: 4 ms (50.85%), Space: 8.9 MB (5.36%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.c
@@ -0,0 +1,8 @@
+int finalValueAfterOperations(char** arr, int n) {
+    int x = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i][0] == '+' || arr[i][1] == '+') x++;
+        else x--;
+    }
+    return x;
+}
