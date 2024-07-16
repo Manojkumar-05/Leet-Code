52627e08cd0936e5266c4bee3874b179df4c9aae
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-16 12:22:52 +0530

    Time: 12 ms (62.27%), Space: 12 MB (5.18%) - LeetHub

---
+++ 3194-find-words-containing-character/3194-find-words-containing-character.c
@@ -0,0 +1,13 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findWordsContaining(char** words, int n, char x, int* rN) {
+    int *arr = (int *)malloc(n * sizeof(int));
+    int idx = 0;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < strlen(words[i]); j++)
+            if(words[i][j] == x) {arr[idx++] = i;break;} 
+        
+    *rN = idx;
+    return arr;
+}
