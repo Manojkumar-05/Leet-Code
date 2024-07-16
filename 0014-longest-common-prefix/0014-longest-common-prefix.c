342d23a6d81249d02bc28e45266d4aa1fd9c868e
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-16 10:24:31 +0530

    Time: 0 ms (100%), Space: 8 MB (5.59%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,20 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+        char *res = (char *)malloc(strlen(strs[0]) + 1);
+        res[0] = '\0';
+        if(strsSize < 2) return strs[0];
+        for(int i = 0; i < strlen(strs[0]); i++){
+            int flag = 1;
+            for(int j = 1; j < strsSize; j++)
+                if(strs[0][i] != strs[j][i]) flag = 0;
+            if(flag){
+                res[i] = strs[0][i];
+                res[i+1] = '\0';
+            }
+            else {
+                printf("%s", res);
+                if(res[0]) return res;
+                return "";
+            }
+        }
+        return res;
+}
