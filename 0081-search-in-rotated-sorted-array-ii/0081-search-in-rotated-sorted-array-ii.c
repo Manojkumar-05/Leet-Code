25bdda74a813b2f3deda70676a246765f303f483
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-17 08:31:49 +0530

    Time: 4 ms (51.66%), Space: 8.3 MB (5.54%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.c
@@ -0,0 +1,23 @@
+int findPiv(int *arr, int n){
+    for(int i = 0; i < n - 1; i++) if(arr[i] > arr[i+1]) return i;
+    return -1;
+}
+bool bs(int *arr, int start, int end, int target){
+    while(start <= end){
+        int mid = start + (end - start) / 2;
+        printf("mid = %d ", mid);
+        if(arr[mid] == target) return true;
+        else if(arr[mid] > target) end = mid - 1;
+        else start = mid + 1;
+    }
+    return false;
+}
+bool search(int* arr, int n, int target) {
+    int piv = findPiv(arr, n);
+    printf("%d ", piv);
+    if(n == 1 && arr[0] == target || arr[0] == target) return true;
+    else if(piv == 0) return bs(arr, piv + 1, n - 1, target); 
+    else if(bs(arr, 0, piv, target) == true) return true;
+    else if(bs(arr, piv+1, n-1, target) == true) return true;
+    return false; 
+}
