892edf43cc7f972f4dea9f55218d57c5afce9c4d
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-17 21:55:35 +0530

    Time: 78 ms (72.57%), Space: 14.8 MB (5.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.c
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -1,8 +1,8 @@
 int maxSubArray(int* arr, int n) {
     int max = arr[0], sum = 0;
     for(int i = 0; i < n; i++){
-        sum += arr[i]; 
         if(sum < 0) sum = 0;
+        sum += arr[i]; 
         if(sum > max) max = sum;
     }
     return max;
