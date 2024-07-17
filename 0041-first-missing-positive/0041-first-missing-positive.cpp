a90ff5377bd8b6b318a8780ad55f2b5eaf7135a9
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-17 11:54:24 +0530

    Time: 181 ms (5.14%), Space: 80 MB (5.95%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,8 +4,7 @@ public:
         map<int, int>m;
         for(auto i : arr) if(i > 0) m[i]++;
         int i = 1;
-        while(i)
-            if(m[i++] < 1) return --i;
+        while(i) if(m[i++] < 1) return --i;
         return 0;
     }
 };
