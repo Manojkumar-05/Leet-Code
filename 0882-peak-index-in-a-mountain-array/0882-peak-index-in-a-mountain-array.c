64ec3bf9b68b8b9faf580efc325fe093eee45dbc
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-17 18:09:24 +0530

    Time: 81 ms (28.86%), Space: 14.3 MB (6.97%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.c
@@ -0,0 +1,9 @@
+int peakIndexInMountainArray(int* arr, int n) {
+    int start = 0, end = n - 1;
+    while(start < end){ 
+        int mid = start + (end - start) / 2;
+        if(arr[mid] < arr[mid + 1]) start = mid + 1;
+        else end = mid;
+    }
+    return end;
+}
