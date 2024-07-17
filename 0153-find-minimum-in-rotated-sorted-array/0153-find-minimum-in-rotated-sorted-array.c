805d3e245a93edcbc787538fdc10ce61158b1080
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-17 08:54:02 +0530

    Time: 0 ms (100%), Space: 8.3 MB (6.79%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.c
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.c
@@ -12,7 +12,6 @@ int minBS(int *arr, int n){
 }
 int findMin(int* arr, int n) {
     if(n == 1) return arr[0];
-    // if(n == 2 && arr[0] > arr[1]) return arr[1];
     int min = minBS(arr, n);
     return min;
 }
