cbe2fe008dd83c4586aaeec068074b0bc886c38f
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-18 11:22:54 +0530

    Time: 41 ms (6.1%), Space: 11.4 MB (6.1%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.c
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.c
@@ -1,14 +1,19 @@
 int isOdd(char n){
-    if((n - '0') % 2 == 1) return 1;
+    int a=(int)(n - '0') % 2 == 1;
+    printf("-%d--%d-%c-\n",a,n,n);
+    if(a) return 1;
     return 0;
 }
 char* largestOddNumber(char* s) {
     int n = strlen(s);
-    int st=0, en=n, j = 0;
+    printf("%d",n);
+    int st, en, j = 0;
     if(isOdd(s[n-1])) return s;
-    char* ans = (char*)malloc(en - st + 2);
     // for(st = 0; st < n; st++) if(isOdd(s[st])) break;
     for(en = n; en >= 0; en--) if(isOdd(s[en])) break;
+    if(st==n)return "";
+    printf("%d, %d, %d", n, st, en);
+    char* ans = (char*)malloc(en - st + 2);
     for(int i = 0; i <= en; i++,j++) ans[j]=s[i];
     ans[j] = '\0';
     return ans;
