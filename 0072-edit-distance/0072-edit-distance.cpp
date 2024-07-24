eba8bbebed68b543e578e72af03de13f4a9a869c
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-24 12:08:01 +0530

    Time: 0 ms (100%), Space: 10.9 MB (82.05%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    // int min(int a, int b, int c){
-    //     if(a < b && a < c) return a;
-    //     else if(b < a && b < c) return b;
-    //     return c;
-    // }
+    int min(int a, int b, int c){
+        if(a < b && a < c) return a;
+        else if(b < a && b < c) return b;
+        return c;
+    }
     int minDistance(string word1, string word2) {
         int l1 = word1.size(), l2 = word2.size();
         int arr[l1+1][l2+1];
@@ -13,7 +13,7 @@ public:
                 if(i == 0) arr[i][j] = j;
                 else if(j == 0) arr[i][j] = i;
                 else if(word1[i - 1] == word2[j - 1]) arr[i][j] = arr[i - 1][j - 1];
-                else arr[i][j] = 1 + std::min({arr[i][j-1], arr[i-1][j], arr[i-1][j-1]});
+                else arr[i][j] = 1 + min(arr[i][j-1], arr[i-1][j], arr[i-1][j-1]);
             }
         } 
         return arr[l1][l2];
