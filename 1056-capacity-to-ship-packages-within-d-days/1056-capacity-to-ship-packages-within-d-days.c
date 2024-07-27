825f609a3330c0e2ce8b80fb49ee78eeaf5f0c5c
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-07-27 13:32:53 +0530

    Time: 43 ms (35.14%), Space: 11.2 MB (5.41%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.c
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.c
@@ -23,9 +23,8 @@ int shipWithinDays(int* arr, int n, int days) {
     int min = mini(arr, n), max = maxi(arr, n);
     while(min <= max){
         int mid = (min + max) / 2;
-        if(daysreq(arr, n, mid) < days){
-            max = mid - 1;
-        }else min = mid + 1;
+        if(daysreq(arr, n, mid) < days) max = mid - 1;
+        else min = mid + 1;
     }
     return min;
 }
