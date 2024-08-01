07df97e427183ba5581f366148e60235f2d20c0f
Manoj <133344712+Manojkumar-05@users.noreply.github.com>
2024-08-01 10:03:23 +0530

    Time: 1043 ms (35.17%), Space: 7.6 MB (8.09%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.c
+++ 0507-perfect-number/0507-perfect-number.c
@@ -1,6 +1,6 @@
 bool checkPerfectNumber(int num) {
-    int copy = num, sum = 0;
+    int sum = 0;
     for(int i = 1; i <= num / 2; i++)
         if(num % i == 0) sum += i;
-    return sum == copy;
+    return sum == num;
 }
